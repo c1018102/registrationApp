330cca0e96939caae79bbd7d526e5bc97819bd15
c1018102 <c1018102@hallam.shu.ac.uk>
2024-01-06 12:41:57 +0000

    CSS

--- moduleregistrationsystem/views.py
+++ moduleregistrationsystem/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'moduleregistrationsystem/home.html')
+    return render(request, 'moduleregistrationsystem/home.html', {'title': 'Welcome'})
 def aboutus(request):
-    return render(request, 'moduleregistrationsystem/aboutus.html')
+    return render(request, 'moduleregistrationsystem/aboutus.html', {'title': 'About'})
 def contactus(request):
-    return render(request, 'moduleregistrationsystem/contactus.html')
+    return render(request, 'moduleregistrationsystem/contactus.html', {'title': 'Contact'})
 def modules(request):
-    return render(request, 'moduleregistrationsystem/modules.html')
+    return render(request, 'moduleregistrationsystem/modules.html', {'title': 'Modules'})
 
 
 
