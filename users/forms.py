2e5c98bbf20bb19980787ae85707a6d665b2d433
c1018102 <c1018102@hallam.shu.ac.uk>
2024-01-07 23:17:12 +0000

    Profile Additional Fields

--- users/forms.py
+++ users/forms.py
@@ -1,21 +1,36 @@
 from django import forms
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Profile
 
-class UserRegisterForm (UserCreationForm):
-    email = forms.EmailField(label= 'Email Address', help_text= 'Your Student Email Address.')    
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(label='Email Address', help_text='Your Student Email Address.')
+    date_of_birth = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), help_text='Your Date of Birth')
+    address = forms.CharField(max_length=255, help_text='Your Address')
+    city_town = forms.CharField(max_length=100, help_text='Your City/Town')
+    country = forms.CharField(max_length=100, help_text='Your Country')
+
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'date_of_birth', 'address', 'city_town', 'country', 'password1', 'password2']
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
+
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
-     model = Profile
-     fields = ['image']
+        model = Profile
+        fields = ['date_of_birth', 'address', 'city_town', 'country', 'image']
+
+    def __init__(self, *args, **kwargs):
+        super(ProfileUpdateForm, self).__init__(*args, **kwargs)
+        
+        # Add required attribute to specific fields
+        self.fields['date_of_birth'].required = True
+        self.fields['address'].required = True
+        self.fields['city_town'].required = True
+        self.fields['country'].required = True
