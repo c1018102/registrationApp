7f10adc05c6fd3eeea0a269656e179c66a1747df
c1018102 <c1018102@hallam.shu.ac.uk>
2024-01-07 02:03:03 +0000

    profile picture default

--- moduleapps/settings.py
+++ moduleapps/settings.py
@@ -119,7 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+MEDIA_ROOT = '/home/student/registrationApp/media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
